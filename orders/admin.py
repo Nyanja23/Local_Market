aa00466239c18785c0d0bc2835c011dd58a36a80
Nyanja23 <josephmendes256@gmail.com>
2025-09-21 13:07:14 +0300

    Added Order and OrderProduct Models

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Order, OrderProduct
 
-# Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'product', 'date_created','complete_status')
+    search_fields = ('customer','product')
+
+@admin.register(OrderProduct)
+class OrderProductAdmin(admin.ModelAdmin):
+    list_display = ('order', 'product', 'quantity')
+    search_fields= ('order', 'product', 'quantity')
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from accounts.models import CustomUser
+from products.models import Product
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Order(models.Model):
+    customer = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, limit_choices_to={'role':'customer'})
+    product = models.ForeignKey(Product, on_delete= models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    complete_status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.customer.username} ordered for {self.product}"
+
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    quantity  = models.PositiveSmallIntegerField(default=1, validators=[MinValueValidator(1)])
+
+    @property
+    def amount_pay(self):
+        product_price = self.product.price
+        return product_price*self.quantity
+    
+    @property
+    def customer(self):
+        return self.order.customer.username
+    
+    @property
+    def vendor(self):
+        return self.product.vendor.username
+
+    def __str__(self):
+        return f"{self.product.name} Ordered By {self.customer}"
